d56eeb5b6f4317f8e07b9d3d81d5f85a56e20bea
Annie Bauer <overheads165@gmail.com>
2022-01-16 20:46:39 +0300

    работающая регистрация

--- public/loginPage.js
+++ public/loginPage.js
@@ -2,13 +2,31 @@
 
 let userForm = new UserForm();
 userForm.loginFormCallback = (data) => {
-  ApiConnector.login({ login: data.login, password: data.password }, responseHandler);
+  let responseHandler = (response) => {
+    if (response.success == true) {
+      location.reload();
+    } else {
+      userForm.setLoginErrorMessage(response.error);
+    }
+  };
+
+  ApiConnector.login(
+    { login: data.login, password: data.password },
+    responseHandler
+  );
 };
 
-let responseHandler = (response) => {
-  if (response.success == true) {
-    location.reload()
-  } else {
-    userForm.setLoginErrorMessage(response.error)
-  }
+userForm.registerFormCallback = (data) => {
+  let registrationHandler = (response) => {
+    if (response.success == true) {
+      location.reload();
+    } else {
+      userForm.setRegisterErrorMessage(response.error);
+    }
+  };
+
+  ApiConnector.register(
+    { login: data.login, password: data.password },
+    registrationHandler
+  );
 };
