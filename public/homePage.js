dd0a19404199d11fd7fc39c12bbdaf7771983eb8
Annie Bauer <overheads165@gmail.com>
2022-01-16 21:46:55 +0300

    получение текущих курсов валют

--- public/homePage.js
+++ public/homePage.js
@@ -1,3 +1,4 @@
+// Кнопка выхода
 let logoutButton = new LogoutButton();
 logoutButton.action = () => {
   let logoutHandler = (response) => {
@@ -9,8 +10,23 @@ logoutButton.action = () => {
   ApiConnector.logout(logoutHandler);
 };
 
+// Отображение профиля
 ApiConnector.current((response) => {
   if (response.success == true) {
     ProfileWidget.showProfile(response.data);
   }
 });
+
+// Отображение курсов валют
+let ratesBoard = new RatesBoard();
+let getStocks = () => {
+  ApiConnector.getStocks((response) => {
+    if (response.success == true) {
+      ratesBoard.clearTable();
+      ratesBoard.fillTable(response.data);
+    }
+  });
+};
+
+getStocks();
+setInterval(getStocks, 60000);
