68683529698ab92840015ff4cc3499a6ab8008f0
Annie Bauer <overheads165@gmail.com>
2022-01-16 20:29:22 +0300

    успешный логин

---
+++ public/loginPage.js
@@ -0,0 +1,12 @@
+"use strict";
+
+let userForm = new UserForm();
+userForm.loginFormCallback = (data) => {
+  ApiConnector.login({ login: data.login, password: data.password }, responseHandler);
+};
+
+let responseHandler = (response) => {
+  if (response.success == true) {
+    location.reload();
+  }
+};
